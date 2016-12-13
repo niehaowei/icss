d70516dc7c9d438c21af026798eabad272b2b40e
niehaowei <1009447515@qq.com>
2016-12-13 14:48:16 +0800

    niehw

---
+++ webservice/src/com/icss/dao/hbase/ScanBean.java
@@ -0,0 +1,76 @@
+package com.icss.dao.hbase;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 用户封装查询结果
+ */
+public class ScanBean {
+	
+	public List<Map<String, String>> results = new ArrayList<Map<String, String>>();
+	
+	public String recordNum;
+	
+	public String startRowKey;
+	
+	public String stopRowKey;
+	
+	public String output;
+	
+	public String fileName;
+	
+	public String retMsg;
+	
+	public String retCode;
+	
+	public String getRetCode() {
+		return retCode;
+	}
+	public void setRetCode(String retCode) {
+		this.retCode = retCode;
+	}
+	public String getOutput() {
+		return output;
+	}
+	public void setOutput(String output) {
+		this.output = output;
+	}
+	public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	public String getRetMsg() {
+		return retMsg;
+	}
+	public void setRetMsg(String retMsg) {
+		this.retMsg = retMsg;
+	}
+	public List<Map<String, String>> getResults() {
+		return results;
+	}
+	public void setResults(List<Map<String, String>> results) {
+		this.results = results;
+	}
+	public String getRecordNum() {
+		return recordNum;
+	}
+	public void setRecordNum(String recordNum) {
+		this.recordNum = recordNum;
+	}
+	public String getStartRowKey() {
+		return startRowKey;
+	}
+	public void setStartRowKey(String startRowKey) {
+		this.startRowKey = startRowKey;
+	}
+	public String getStopRowKey() {
+		return stopRowKey;
+	}
+	public void setStopRowKey(String stopRowKey) {
+		this.stopRowKey = stopRowKey;
+	}
+}
